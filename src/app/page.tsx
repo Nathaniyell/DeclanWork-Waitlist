7f9d892a5baa4eb338995e95e14cacfea3ac77c2
Nathaniyell <essien.nathan@yahoo.com>
2023-11-09 12:32:59 +0100

    add logic to handle form submit and input change events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 import Image from "next/image";
 import logo from "../../public/assets/Images/logo.png";
 import mockup from "../../public/assets/Images/mockup.png";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((previousFormValues) => ({
+      ...previousFormValues,
+      [name]: value,
+    }));
+  };
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData)
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+    })
+  };
   return (
     <main className="min-h-screen relative">
       <Image src={logo} alt="Logo" className="absolute inline left-10 top-4 md:left-20" />
       <div className="flex items-center justify-between flex-col lg:flex-row bg-white">
-        <form action="" className="p-8 flex-1 relative top-20 md:top-14 lg:top-8 mb-10">
+        <form onSubmit={handleFormSubmit} className="p-8 flex-1 relative top-20 md:top-14 lg:top-8 mb-10">
           <div className="flex items-start justify-between flex-col h-[150px] md:h-[100px] lg:md:h-[120px] bg-white">
             <span className="bg-[#1671D9] text-white p-1 text-sm rounded-lg md:text-xs">
               Coming soon
@@ -25,6 +50,9 @@ export default function Home() {
                 type="text"
                 id="firstName"
                 name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+
                 placeholder="John"
                 className="border border-black p-2 w-full indent-1 mt-2 outline-[#00D37B] outline-1"
                 required
@@ -36,6 +64,9 @@ export default function Home() {
                 type="text"
                 id="lastName"
                 name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+
                 placeholder="Doe"
                 className="border border-black p-2 w-full indent-1 mt-2 outline-[#00D37B] outline-1"
                 required
@@ -47,6 +78,9 @@ export default function Home() {
             type="email"
             id="email"
             name="email"
+            value={formData.email}
+            onChange={handleChange}
+
             placeholder="johndoe@example.com"
             className="border border-black p-2  indent-2 w-full mt-2 outline-[#00D37B] outline-1"
             required
