755f24133d850c41d25a6218a3addf0a766d7d01
Nathaniyell <essien.nathan@yahoo.com>
2023-11-09 09:02:03 +0100

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,20 @@ export default function Home() {
     <main className="min-h-screen relative">
   <Image src={logo} alt="Logo" className="absolute inline left-10 top-4" />
   <div className="flex items-center justify-between flex-col md:flex-row bg-white">
-    <form action="" className="p-8 bg-red-500 flex-1 relative">
-      <div className="flex items-start justify-between flex-col h-[100px] bg-white">
+    <form action="" className="p-8 flex-1 relative top-20">
+      <div className="flex items-start justify-between flex-col h-[120px] bg-white">
       <span className="bg-[#1671D9] text-white p-1 rounded-lg text-sm">Coming soon</span>
       <h1 className="text-xl text-black">Get Early Access</h1>
-      <p>Be one of the first to get notified when we launch our Beta version</p>
+      <p className="text-[#757575] w-4/5">Be one of the first to get notified when we launch our Beta version</p>
       </div>
+      <section className="bg-green-600 mt-8">
+
+      <label htmlFor="firstName">First Name</label>
+      </section>
     </form>
-    <div className="bg-[#EDFCF6]">
+    {/* <div className="bg-[#EDFCF6]">
       <Image src={mockup} alt="Mock up" className="" />
-    </div>
+    </div> */}
   </div>
     </main>
   )
