77766da9b9034f31848c18c88f4824a83fa6439a
Nathaniyell <essien.nathan@yahoo.com>
2023-11-09 08:24:39 +0100

    update page.tsx and tailwind.config.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import logo from "../../public/assets/Images/logo.png"
+
 export default function Home() {
   return (
-    <main className="min-h-screen p-10">
-      
+    <main className="min-h-screen p-6 ">
+  <Image src={logo} alt="Logo" />
     </main>
   )
 }
